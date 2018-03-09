c7fab1d96738fa875859f42fadb3367cc7c8e2c3
plasmax <tsalxam@gmail.com>
2018-03-09 20:12:17 +0000

    Update qt.py

--- PythonEditor/qt.py
+++ PythonEditor/qt.py
@@ -1,12 +1,20 @@
-import sys
 try:
     from PySide import QtGui, QtCore
-except ImportError:
+except ImportError, e:
+    import sys
     try:
-        sys.path.append('/usr/lib64/python2.7/site-packages/')
-        from PyQt4 import QtGui, QtCore
-        QtCore.Signal = QtCore.pyqtSignal
-        QtCore.Slot = QtCore.pyqtSlot
-    except ImportError:
-        sys.path.append('C:/Program Files/Nuke10.5v1/pythonextensions/site-packages')
+        print e, ', trying nuke PySide installation'
+        sys.path.append('/opt/foundry/nuke-10.0v4/pythonextensions/site-packages/')
         from PySide import QtGui, QtCore
+        print QtGui, QtCore
+    except ImportError:
+        try:
+            print e, ', defaulting to PyQt'
+            sys.path.append('/usr/lib64/python2.7/site-packages/')
+            from PyQt4 import QtGui, QtCore
+            QtCore.Signal = QtCore.pyqtSignal
+            QtCore.Slot = QtCore.pyqtSlot
+        except ImportError:
+            print e, ', trying windows nuke PySide installation'
+            sys.path.append('C:/Program Files/Nuke10.5v1/pythonextensions/site-packages')
+            from PySide import QtGui, QtCore
