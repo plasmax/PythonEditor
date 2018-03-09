4904bf6b8cf32707063e0b86a34bae217336674a
plasmax <tsalxam@gmail.com>
2018-03-09 20:10:38 +0000

    Update IDE.py

--- PythonEditor/IDE.py
+++ PythonEditor/IDE.py
@@ -3,12 +3,14 @@ import os
 import sys
 from functools import partial
 
+print(sys.version)
 import time
 print('importing', __name__, 'at', time.asctime())
 
 SIMULATION = 'global_test_confirmed'
 
 from qt import QtGui, QtCore
+
 from constants import NUKE_DIR
 
 from browser import NukeMiniBrowser
@@ -19,6 +21,7 @@ class IDE(QtGui.QWidget):
     def __init__(self):
         super(IDE, self).__init__()
         self.layout = QtGui.QVBoxLayout(self)
+        self.setToolTip('Python Editor')
         self._setup()
 
     def _setup(self):
@@ -43,6 +46,8 @@ class IDE(QtGui.QWidget):
             self.toptab.addTab(self.output, 'Output')
             self.toptab.addTab(self.browser, 'Browser')
             self.toptab.addTab(QtGui.QTreeWidget(), 'Object Inspector')
+            self.toptab.addTab(QtGui.QWidget(), 'Preferences')
+            self.toptab.addTab(QtGui.QWidget(), 'Recent Files List')
             self.splitter.addWidget(self.toptab)
 
         self.splitter.addWidget(self.input)
@@ -78,7 +83,7 @@ if __name__ == '__main__':
         import qdarkstyle
         app.setStyleSheet(qdarkstyle.load_stylesheet_pyside())
     except ImportError:
-        sys.path.append('/net/homes/mlast/.nuke/python/max/SublimeNuke/qdarkstyle')
+        sys.path.append('/net/homes/mlast/.nuke/python/max/PythonEditor/qdarkstyle')
         import qdarkstyle
         app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt())
         print('QDarkStyle not found')
