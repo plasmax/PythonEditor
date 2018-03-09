3001fc37be1085640e2291e9220ff0599c02e2a7
plasmax <tsalxam@gmail.com>
2018-03-09 20:08:43 +0000

    Update autocompletion.py

--- PythonEditor/editor/features/autocompletion.py
+++ PythonEditor/editor/features/autocompletion.py
@@ -44,52 +44,74 @@ class CodeEditorAuto(CodeEditor):
         textCursor.select(QtGui.QTextCursor.WordUnderCursor)
         return textCursor.selectedText()
 
-    def dotLastWord(self):
+    def getObjectBeforeChar(self, _char):
         self.completer.setCompletionPrefix('')
         textCursor = self.textCursor()
-        textCursor.select(QtGui.QTextCursor.LineUnderCursor)#BlockUnderCursor
-        # textCursor.select(QtGui.QTextCursor.WordUnderCursor)
+
+        textCursor.select(QtGui.QTextCursor.LineUnderCursor)
         selectedText = textCursor.selectedText()
-        print selectedText
-        print 'RE:', re.findall('(\w+\.)+', selectedText),
+
         lastword = selectedText.split(' ')[-1]
+        word_before_dot = '.'.join(lastword.split('.')[:-1])
 
-        dotLastWord = '.'.join(lastword.split('.')[:-1])
-
-        if (dotLastWord.strip() == '' 
-            or dotLastWord.endswith('.')):
+        if (word_before_dot.strip() == '' 
+                or word_before_dot.endswith(_char)
+                or not word_before_dot[-1].isalnum()):
             return
 
         _objects = self._globals.copy()
         _objects.update(self._locals.copy())
 
-        if dotLastWord in _objects:
-            _obj = _objects.get(dotLastWord)
+        varname_exists = (word_before_dot in _objects)
+
+        if varname_exists:
+            _obj = _objects.get(word_before_dot)
         else:
             try:
-                exec('_obj = '+dotLastWord, self._globals, self._locals)
-                _obj = self._locals.get('_obj')
-            except Exception, e:
-                print 'FAIL!', e
+                loc = {}
+                exec('_obj = '+word_before_dot, self._globals, loc)#self._locals)
+                print loc
+                _obj = loc.get('_obj')
+            except NameError, e:
+                print e
                 return
 
         print _obj
+        return _obj
+
+    def completeObject(self, _obj=None):
+
+        if _obj == None:
+            _obj = self.getObjectBeforeChar('.')
+
+        if _obj is None or False:
+            return
+
         attrs = dir(_obj)
 
         methods = [a for a in attrs if a[0].islower()]
         therest = [a for a in attrs if not a[0].islower()]
+        stringlist = methods+therest
+        self.setList(stringlist)
+        self.showPopup()
+
+    def completeVariables(self):
+        pass
+
+    def setList(self, stringlist):
         qslm =  QtGui.QStringListModel()
-        qslm.setStringList(methods+therest)
+        qslm.setStringList(stringlist)
         self.completer.setModel(qslm)
 
+    def showPopup(self):
         cursorRect = self.cursorRect()
         cursorRect.setWidth(self.completer.popup().sizeHintForColumn(0)
             + self.completer.popup().verticalScrollBar().sizeHint().width())
         self.completer.complete(cursorRect)
-        
 
     def insertCompletion(self, completion):
         # print 'prefix:', self.completer.completionPrefix()
+        print completion
         textCursor = self.textCursor()
         extra = (len(completion) -
             len(self.completer.completionPrefix()))
@@ -100,8 +122,9 @@ class CodeEditorAuto(CodeEditor):
 
     def keyPressEvent(self, event):
         cp = self.completer
+        cpActive = cp and cp.popup() and cp.popup().isVisible()
 
-        if cp and cp.popup() and cp.popup().isVisible():
+        if cpActive:
             if event.key() in (
                                 QtCore.Qt.Key_Enter,
                                 QtCore.Qt.Key_Return,
@@ -110,18 +133,63 @@ class CodeEditorAuto(CodeEditor):
                                 QtCore.Qt.Key_Backtab):
                 event.ignore()
                 return True
+            elif (not str(event.text()).isalnum()
+                    and event.modifiers() == QtCore.Qt.NoModifier):
+                cp.popup().hide()
+
+        if event.key() == QtCore.Qt.Key_Tab:
+            textCursor = self.textCursor()
+            if not textCursor.hasSelection():
+                textCursor.select(QtGui.QTextCursor.LineUnderCursor)
+                selectedText = textCursor.selectedText()
+                if selectedText.endswith('.'):
+                    print selectedText
+                    self.completeObject()
+                    return True
+                elif selectedText.endswith('('):
+                    # _obj = self.getObjectBeforeChar('(')
+                    # print '\tOBJECT!!!!!!!!'*10, _obj
+                    ret = {}
+                    # cmd = 'import inspect\n'
+                    # cmd += '__ret = inspect.getargspec('+ selectedText[:-1] +')'
+                    cmd = '__ret = '+ selectedText[:-1].split(' ')[-1]
+                    print cmd
+                    cmd = compile(cmd, '<Python Editor Tooltip>', 'exec')
+                    exec(cmd, self._globals, ret)
+                    _obj = ret.get('__ret')
+                    if _obj:
+                        print 'returned object:', _obj
+                        info = str('')
+                        import inspect, types #move this to top
+                        if (isinstance(_obj, types.BuiltinFunctionType)
+                                or isinstance(_obj, types.BuiltinMethodType)):
+                            info = _obj.__doc__
+                        else:
+                            info = str(inspect.getargspec(_obj))
+                            if _obj.__doc__:
+                                info += '\n' + _obj.__doc__
+
+                        QtGui.QToolTip.showText(self.mapToGlobal(self.cursorRect().center()), info)
+                    return True
 
         super(CodeEditorAuto, self).keyPressEvent(event)
                 
         if event.key() == QtCore.Qt.Key_Period:
             if cp.popup():
                 cp.popup().hide()
-            self.dotLastWord()
+            self.completeObject()
+            # self.getObjectBeforeChar('.')
+            return True
 
-        elif event.text().isalpha():
+        elif (cp and cp.popup() and cp.popup().isVisible()):
             currentWord = self.wordUnderCursor()
 
             cp.setCompletionPrefix(currentWord)
             cp.popup().setCurrentIndex(cp.completionModel().index(0,0))
 
+        # else:
+            # self.completeVariables()
+
+
+            
 
