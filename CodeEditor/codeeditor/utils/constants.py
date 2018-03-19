51fc920e284975a9c3ad19d66e52bee0af02fd31
plasmax <tsalxam@gmail.com>
2018-03-19 23:09:20 +0000

    Create constants.py

---
+++ CodeEditor/codeeditor/utils/constants.py
@@ -0,0 +1,28 @@
+import sys
+import os
+
+try:
+    import nuke
+    if nuke.NUKE_VERSION_MAJOR > 10:
+        pyside = 'PySide2'
+        pyqt = 'PyQt5'
+    else:
+        raise NotImplementedError
+except Exception:
+    pyside = 'PySide'
+    pyqt = 'PyQt4'
+
+if sys.platform == "linux" or sys.platform == "linux2":
+    # linux
+    path = '/net/homes'
+    PATH_DELIM = ':'
+elif sys.platform == "darwin": # OS X
+    raise NotImplementedError, 'Mac OS currently not supported.'
+elif sys.platform == "win32": # Windows
+    path = 'C:/Users'
+    PATH_DELIM = ';'
+
+USER = os.environ.get('USERNAME')
+NUKE_DIR = '/'.join([path, USER, '.nuke'])
+AUTOSAVE_FILE = NUKE_DIR + '/PythonEditorHistory.xml'
+QT_VERSION = pyside + PATH_DELIM + pyqt
