5e6e6c71d08c2ac7b600651ed9991f71d84b9b12
plasmax <tsalxam@gmail.com>
2018-03-27 19:17:36 +0100

    Create objectinspector.py

---
+++ PythonEditor/ui/objectinspector.py
@@ -0,0 +1,86 @@
+import os
+import inspect
+from __main__ import __dict__
+
+from codeeditor.utils.constants import QT_VERSION
+
+import os
+os.environ['QT_PREFERRED_BINDING'] = 'PySide:PyQt4'
+
+from Qt import QtWidgets, QtCore, QtGui
+
+class ObjectInspector(QtWidgets.QWidget):
+    """
+    Inspired by the Object Inspector from 
+    the Pythonista app.
+    """
+    pass
+    def __init__(self):
+        super(ObjectInspector, self).__init__()
+        self.layout = QtWidgets.QGridLayout(self)
+
+        self.setMinimumWidth(900)
+        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
+
+        self.treeview = QtWidgets.QTreeView()
+        self.treemodel = QtGui.QStandardItemModel()
+        self.treeview.setModel(self.treemodel)
+        self.treeview.setUniformRowHeights(True)
+
+        self.layout.addWidget(self.treeview)
+
+        self.treemodel.setHorizontalHeaderLabels(['object name', 
+                                                'object'])
+
+        self.treeview.header().setStretchLastSection(False)
+        self.treeview.header().setResizeMode(QtWidgets.QHeaderView.ResizeToContents)
+
+        self.load_globals()
+        self.start_timer()
+
+    def load_globals(self):
+        """
+        Load globals into Tree
+        """
+        self.treemodel.removeRows( 0, self.treemodel.rowCount() )
+
+        self.names = __dict__.copy()
+
+        rootItem = self.treemodel.invisibleRootItem()
+        for key, value in __dict__.iteritems():
+            # if hasattr(value, '__repr__'):
+            try:
+                items = [QtGui.QStandardItem(i.__repr__()) 
+                        for i in [key, value]]
+                rootItem.appendRow(items)
+            except Exception, e:
+                print key, value, e
+
+
+    def start_timer(self):
+        """
+        Starts timer for 
+        self.check_update_globals
+        """
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(100)
+        self.timer.timeout.connect(self.check_update_globals)
+        self.timer.start()
+        
+    def check_update_globals(self):
+        """
+        Timer that checks len() of 
+        __main__.__dict__
+        and updates globals on new items.
+        """
+        if not self.names == __dict__:
+            self.load_globals()
+
+    def open_in_sublime(self):
+        """
+        Like sublime's "Go to"
+        Open file in which object was defined.
+        TODO: Would be excellent to jump to line.
+        """
+
+
