6404620dcb8af34a69cc697247cba374a6ca72fa
plasmax <tsalxam@gmail.com>
2018-03-28 22:32:24 +0100

    Update nukedock.py

--- PythonEditor/ui/nukefeatures/nukedock.py
+++ PythonEditor/ui/nukefeatures/nukedock.py
@@ -1,22 +1,4 @@
-"""
-For menu.py usage:
-from PythonEditor.ui.features import nukedock
-nukedock.setup_dock()
-"""
-import time
 import sys
-from PythonEditor.utils.constants import NUKE_DIR
-
-reloadAllModules ="""
-for m in sys.modules.keys():
-    if 'PythonEditor' in m:
-        del sys.modules[m]
-import PythonEditor
-reload(PythonEditor)
-from PythonEditor.ui.nukefeatures import nukedock
-nukedock.setup_dock()
-nukescripts.panels.__panels["i.d.e.Python_Editor"]()
-"""
 
 def setup_dock():
     try:
@@ -53,15 +35,6 @@ def setup_dock():
     #         return None
     #     registerWidgetAsPanel( widget, name, id, create = False )
 
-    registerWidgetAsPanel('__import__("PythonEditor").ide.IDE', "Python Editor", 'i.d.e.Python_Editor')
-
-    panelMenu = nuke.menu('Nuke').addMenu('Panels')
-    panelMenu.addCommand('Python Editor', reloadAllModules,
-        '\\', icon= NUKE_DIR + '/icons/PythonEditor.png')
-
-    nuke.menu('Nodes').addCommand('Python Editor', 
-        'nukescripts.panels.__panels["i.d.e.Python_Editor"]()', 
-        'Alt+z', icon= NUKE_DIR + '/icons/PythonEditor.png')
-
-if NUKE_DIR in sys.path:
-    setup_dock()
+    registerWidgetAsPanel('__import__("PythonEditor").ide.IDE',
+                          "Python Editor", 
+                          'i.d.e.Python_Editor')
