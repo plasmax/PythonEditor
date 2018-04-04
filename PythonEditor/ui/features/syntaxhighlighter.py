af01be065fc3fdf529e2e7dbcc58d8a458a871e2
plasmax <tsalxam@gmail.com>
2018-04-04 21:43:20 +0100

    Update syntaxhighlighter.py

--- PythonEditor/ui/features/syntaxhighlighter.py
+++ PythonEditor/ui/features/syntaxhighlighter.py
@@ -32,7 +32,7 @@ class Highlight(QtGui.QSyntaxHighlighter):
             'del', 'elif', 'else', 'except', 'exec', 'finally',
             'for', 'from', 'global', 'if', 'import', 'in',
             'is', 'lambda', 'not', 'or', 'pass', 'print',
-            'raise', 'return', 'try', 'while', 'yield',
+            'raise', 'return', 'try', 'while', 'yield', 'with'
             ]
 
         self.operatorKeywords = [
