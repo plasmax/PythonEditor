536f336a42076e2d6f461e6d40da8197fe786be9
plasmax <tsalxam@gmail.com>
2018-04-05 23:23:52 +0100

    Update ide.py

--- PythonEditor/ui/ide.py
+++ PythonEditor/ui/ide.py
@@ -1,110 +1,57 @@
 import sys
-from functools import partial
-
-from Qt import QtWidgets, QtCore, QtGui
-from editor import Editor
-from terminal import Terminal
-from features import shortcuts
-from PythonEditor.utils import save
-from PythonEditor.utils import constants
-from PythonEditor.ui.features import filehandling
+import imp
+from Qt import QtWidgets
+from PythonEditor.ui import pythoneditor
+from PythonEditor.ui import pythoneditortabs
 
 class IDE(QtWidgets.QWidget):
     """
-    Main widget. Sets up layout 
-    and connects some signals.
+    Container widget that allows the
+    whole package to be reloaded for 
+    development purposes.
     """
     def __init__(self):
         super(IDE, self).__init__()
+        self.layout = QtWidgets.QHBoxLayout(self)
+        self.layout.setContentsMargins(0,0,0,0)
         self.setObjectName('IDE')
+        self.buildUI()
 
-        self.construct_ui()
-        self.connect_signals()
+    def buildUI(self, tabs=False):
+        if tabs:
+            self.pythonEditor = pythoneditortabs.PythonEditorTabs(parent=self)
+        else:
+            self.pythonEditor = pythoneditor.PythonEditor(parent=self)
+        self.layout.addWidget(self.pythonEditor)
 
-    def construct_ui(self):
-        layout = QtWidgets.QVBoxLayout(self)
-        layout.setObjectName('IDE_MainLayout')
-        layout.setContentsMargins(0,0,0,0)
-
-        splitter = QtWidgets.QSplitter(QtCore.Qt.Vertical)
-        splitter.setObjectName('IDE_MainVerticalSplitter')
-
-        self.editor = Editor(handle_shortcuts=True)
-        self.filehandler = filehandling.FileHandler(self.editor)
-        self.terminal = Terminal()
-
-        editor_path = constants.get_external_editor_path()
-        if not editor_path:
-            constants.set_external_editor_path()
-
-        self.setup_menu()
-
-        splitter.addWidget(self.terminal)
-        splitter.addWidget(self.editor)
-
-        layout.addWidget(splitter)
-
-    def connect_signals(self):
+    def reload_package(self, tabs=False):
         """
-        Connect child widget slots to shortcuts.
-        TODO: Find a better place to set this up,
-        as the shortcuthandler will not be so readily 
-        available when the editor is placed inside a 
-        TabWidget. Could relay the signal through the 
-        TabWidget, or find a global connection mechanism.
-
-        Alternatively, we can dynamically set ShortcutHandler's
-        editor widget depending on the TabWidget's current tab.
-        (In this case the QShortcut widget is the tabwidget and
-        ShortcutContext is WidgetWithChildrenShortcut or WindowShortcut). 
-        This would mean keeping the ShortcutHandler here (and avoid 
-        creating new shortcut objects for every tab).
+        Reloads the whole package.
         """
-        rco = self.editor.relay_clear_output_signal
-        rco.connect(self.terminal.clear)
+        self.pythonEditor.deleteLater()
+        del self.pythonEditor
 
-    def setup_menu(self):
-        """
-        Adds top menu bar and various menu items.
 
-        TODO: Implement the following:
-        # fileMenu.addAction('Save') #QtGui.QAction (?)
+        not_reloadable = [
+                            'PythonEditor.ui.idetabs',
+                            'PythonEditor.ui.pythoneditor',
+                            'PythonEditor.ui.ide', 
+                            '__main__'
+                         ]
 
-        # editMenu.addAction('Copy to External Editor')
-        # editMenu.addAction('Open in External Editor')
+        loaded_modules = sys.modules
+        for name, mod in loaded_modules.items():
+            if (mod and hasattr(mod, '__file__')
+                    and 'PythonEditor' in mod.__file__
+                    and name not in not_reloadable):
+                imp.reload(mod)
+                # print name
+                # del sys.modules[name]
 
-        # helpMenu.addAction('About Python Editor')
-        """
-        menuBar = QtWidgets.QMenuBar(self)
-        fileMenu = QtWidgets.QMenu('File')
-        helpMenu =  QtWidgets.QMenu('Help')
-        editMenu =  QtWidgets.QMenu('Edit')
-        
-        for menu in [fileMenu, editMenu, helpMenu]:
-            menuBar.addMenu(menu)
+        reload(pythoneditor)
+        reload(pythoneditortabs)
 
-        save_as = partial(save.save_as, self.editor)
-        fileMenu.addAction('Save As', save_as)
-
-        save_selected = partial(save.save_selected_text, self.editor)
-        fileMenu.addAction('Save Selected Text', save_selected)
-        
-        export_to_external_editor = partial(save.export_selected_to_external_editor, self.editor)
-        fileMenu.addAction('Export Selected To External Editor', export_to_external_editor)
-
-        editMenu.addAction('Preferences') #TODO: Set up Preferences widget with External Editor path option 
-        editMenu.addAction('Shortcuts', self.show_shortcuts)
-
-        helpMenu.addAction('Unload Python Editor', self.reload_package)
-
-        self.layout().addWidget(menuBar)
-
-    def show_shortcuts(self):
-        """
-        Generates a popup dialog listing available shortcuts.
-        TODO: Make this editable, and reassign shortcuts on edit.
-        """
-        self.editor.shortcuteditor.show()
+        self.buildUI(tabs=tabs)
 
     def showEvent(self, event):
         """
@@ -121,25 +68,3 @@ class IDE(QtWidgets.QWidget):
             pass
 
         super(IDE, self).showEvent(event)
-
-    def dragEnterEvent(self, event):
-        """
-        Allow event to pass to the editor.
-        """
-        event.ignore()
-        super(IDE, self).dragEnterEvent(event)
-
-    def reload_package(self):
-        """
-        Reloads the whole package.
-        TODO:
-        Currently unloads only. 
-        Needs to reload.
-        """
-        self.close()
-        loaded_modules = sys.modules
-        for name, mod in loaded_modules.items():
-            if (mod
-                    and hasattr(mod, '__file__') 
-                    and 'PythonEditor' in mod.__file__):
-                del sys.modules[name]
