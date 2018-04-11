57adda71d3dfc420a33d679c67ff2e6309ae1d21
Max Last <tsalxam@gmail.com>
2018-04-11 04:49:07 +0100

    preferences editor now displays current external editor

--- PythonEditor/ui/preferenceseditor.py
+++ PythonEditor/ui/preferenceseditor.py
@@ -1,9 +1,12 @@
-from Qt import QtWidgets, QtCore, QtGui
+import os
+from PythonEditor.ui.Qt import QtWidgets, QtCore, QtGui
+from PythonEditor.utils.constants import set_external_editor_path
 
 class PreferencesEditor(QtWidgets.QTreeView):
     """
     A display widget that allows editing of
     preferences assigned to the editor.
+    TODO: Implement mechanism to change external editor.
     """
     def __init__(self):
         super(PreferencesEditor, self).__init__()
@@ -15,3 +18,12 @@ class PreferencesEditor(QtWidgets.QTreeView):
         self.externalEditorLabel.setBuddy(self.externalEditorPath)
         self.layout.addWidget(self.externalEditorLabel)
         self.layout.addWidget(self.externalEditorPath)
+
+        #TODO: connect externaleditorpath text changed signal (or set prefs on preferences > close?)
+
+    def showEvent(self, event):
+        self.show_current_preferences()
+        super(PreferencesEditor, self).showEvent(event)
+
+    def show_current_preferences(self):
+        self.externalEditorPath.setText(unicode(os.environ.get('EXTERNAL_EDITOR_PATH')))
