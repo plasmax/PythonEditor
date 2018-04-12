0208c0c9f8975efcd1d215cc4d7a96121f9c1c0e
plasmax <tsalxam@gmail.com>
2018-04-13 00:19:03 +0100

    Update nodepanels.py

--- PythonEditor/ui/nukefeatures/nodepanels.py
+++ PythonEditor/ui/nukefeatures/nodepanels.py
@@ -1,20 +1,65 @@
 from functools import partial
 import nuke
 from PythonEditor.ui.Qt import QtWidgets, QtCore, QtGui
+from PythonEditor.ui import editor
 
-class PyKnobEdit(QtWidgets.QPlainTextEdit):
+class PyKnobs(QtWidgets.QWidget):
+    """docstring for PyKnobs"""
+    def __init__(self, knobs):
+        super(PyKnobs, self).__init__()
+        self.knobs = knobs
+        self._knob = knobs[0]
+        self.layout = QtWidgets.QVBoxLayout(self)
+
+        self.knobChooser = QtWidgets.QComboBox()
+        self.knobChooser.addItems([k.name() for k in knobs])
+        self.editor = PyKnobEdit(self._knob)
+
+        self.layout.addWidget(self.knobChooser)
+        self.layout.addWidget(self.editor)
+
+        self.knobChooser.currentIndexChanged.connect(self.updateKnob)
+
+    def updateKnob(self, index):
+        self.editor.knob = self.knobs[index]
+        self.editor.getKnobValue()
+
+class PyKnobEdit(editor.Editor):
+    """
+    Editor that automatically updates knobs.
+    """
     def __init__(self, knob):
         super(PyKnobEdit, self).__init__()
         self._knob = knob
-        self.setPlainText(knob.value())
         self.textChanged.connect(self.updateValue)
 
     def updateValue(self):
-        #prepend = "if nuke.thisKnob().name() != 'knobChanged':"
-        self._knob.setValue(self.toPlainText())        
+        # if self._knob.name() != 'knobChanged':
+        self._knob.setValue(self.toPlainText())
+
+    def hideEvent(self, event):
+        super(PyKnobEdit, self).hideEvent(event)
+
+    @property
+    def knob(self):
+        return self._knob
+
+    @knob.setter
+    def knob(self, knob):
+        self._knob = knob
+        
+    def getKnobValue(self):
+        self.setPlainText(self.knob.value())
 
 @QtCore.Slot(object)
 def addTextKnobs(node):
+    """
+    Finds node panel widget and adds some 
+    extra widgets to it that act like knobs.
+    TODO: find node panels in Properties bin.
+    also appears that this causes segmentation faults
+    probably because of pointers to missing or already deleted widgets.
+    """
     print node.name()
 
     np_list = [w for w in QtWidgets.QApplication.instance().allWidgets()
@@ -27,15 +72,18 @@ def addTextKnobs(node):
     sw = np.findChild(QtWidgets.QStackedWidget, 'qt_tabwidget_stackedwidget')
     print sw
     tw = sw.parent()
-    stw = QtWidgets.QTabWidget() # probably nicer to have a dropdown connected to a single textedit
-    tw.addTab(stw, 'Python Knobs')
+    pyk = PyKnobs([k for k in node.allKnobs() if 'py' in k.Class().lower()])
+    tw.addTab(pyk, 'Python Knobs')
+    
+    # stw = QtWidgets.QTabWidget() #TODO: probably nicer to have a dropdown connected to a single textedit
+    # tw.addTab(stw, 'Python Knobs')
 
-    for k in node.allKnobs():
-        if 'py' in k.Class().lower():
-            stw.addTab(PyKnobEdit(k), k.name())
+    # for k in node.allKnobs():
+    #     if 'py' in k.Class().lower():
+    #         stw.addTab(PyKnobEdit(k), k.name())
 
 def pythonKnobEdit():
-    if nuke.thisKnob().name() == 'showPanel':
+    if nuke.thisKnob().name() == 'showPanel': #TODO: is there a 'knob added' knobchanged?
         node = nuke.thisNode()
         global timer
         timer = QtCore.QTimer()
@@ -47,12 +95,17 @@ def pythonKnobEdit():
 
 
 testing = """
-nuke.pluginAddPath('/net/homes/mlast/.nuke/python/max/gear/tools/')
+from PythonEditor.ui.nukefeatures import nodepanels
+reload(nodepanels)
 
-import M_PyKnobPanel
-reload(M_PyKnobPanel)
-
+#to delete
 del nuke.callbacks.knobChangeds['*'][1]
 """
+
+knobChangeds = nuke.callbacks.knobChangeds['*']
+for index, info in enumerate(knobChangeds):
+    func, _,_,_ = info
+    if func.func_name == 'pythonKnobEdit':
+        del knobChangeds[index]
 
 nuke.callbacks.addKnobChanged(pythonKnobEdit, args=(), kwargs={}, nodeClass='*', node=None)
