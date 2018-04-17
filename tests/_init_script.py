1005e684054304463aa9868a04041d3972aa2863
plasmax <tsalxam@gmail.com>
2018-04-17 20:39:37 +0100

    Update _init_script.py

--- tests/_init_script.py
+++ tests/_init_script.py
@@ -1,13 +1,14 @@
 """
-Executed on nuke startup to load PythonEditor.
+Executed on Nuke startup to load PythonEditor.
 """
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 if ('nuke' in globals() and nuke.GUI):
-    from PythonEditor.ui.nukefeatures import nukeinit
-    nukeinit.setup()
+    import PythonEditor
+    # from PythonEditor.ui.nukefeatures import nukeinit
+    # nukeinit.setup()
     from PythonEditor.utils.constants import NUKE_DIR
     execfile(os.path.join(NUKE_DIR, 'init.py'))     # executes init.py
                                                     # found in user .nuke
