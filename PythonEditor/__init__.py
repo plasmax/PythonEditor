e9448eb02f81e0c52ade157dc135c5827ab8a5fe
plasmax <tsalxam@gmail.com>
2018-04-17 20:39:10 +0100

    Update __init__.py

--- PythonEditor/__init__.py
+++ PythonEditor/__init__.py
@@ -1,6 +1,19 @@
-from PythonEditor.utils.constants import QT_VERSION
-
 import os
+from PythonEditor.utils.constants import QT_VERSION
 os.environ['QT_PREFERRED_BINDING'] = QT_VERSION
 
 from PythonEditor.ui import ide
+
+def nuke_menu_setup():
+    """
+    If in Nuke, setup menu.
+    """
+    try:
+        import nuke
+    except ImportError:
+        return
+
+    from PythonEditor.ui.nukefeatures import nukeinit
+    nukeinit.setup()
+
+nuke_menu_setup()
