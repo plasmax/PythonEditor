a46294e7fe30dcab6eba3fd8bf5f7d74e41109bb
Max Last <tsalxam@gmail.com>
2018-05-17 07:57:49 +0100

    refactoring test

--- tests/test_colour_dialog.py
+++ tests/test_colour_dialog.py
@@ -1,16 +1,19 @@
 from PythonEditor.ui.Qt import QtWidgets, QtGui, QtCore
 
-ide = [w for w in QtWidgets.QApplication.topLevelWidgets() if w.objectName() == 'IDE'][0]
+widgets = QtWidgets.QApplication.topLevelWidgets()
+ide = [w for w in widgets if w.objectName() == 'IDE'][0]
 ide.setStyleSheet('background:rgb(45,42,46);')
+
 
 @QtCore.Slot(QtGui.QColor)
 def info(colour):
     rgb = {'r': colour.red(),
-               'g': colour.green(),
-               'b': colour.blue()}
+           'g': colour.green(),
+           'b': colour.blue()}
     ide.setStyleSheet('background:rgb({r},{g},{b});'.format(**rgb))
+
 
 cd = QtWidgets.QColorDialog()
 cd.currentColorChanged.connect(info)
-#cd.colorSelected.connect(info)
 cd.show()
+# cd.colorSelected.connect(info)
