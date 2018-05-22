0fc5b1e194a89cc0452262d5605dad1f077404a6
Max Last <tsalxam@gmail.com>
2018-05-22 08:40:32 +0100

    continued testing of treeview

--- tests/test_treeview.py
+++ tests/test_treeview.py
@@ -6,7 +6,8 @@ PACKAGE_DIR = dn(TESTS_DIR)
 folder = PACKAGE_DIR
 sys.path.append(folder)
 
-from PythonEditor.ui import editor
+#from PythonEditor.ui import editor
+from PythonEditor.ui import edittabs
 from PythonEditor.ui import browser
 from PythonEditor.ui.Qt import QtWidgets, QtGui, QtCore
 reload(browser)
@@ -16,57 +17,87 @@ class Manager(QtWidgets.QWidget):
     def __init__(self):
         super(Manager, self).__init__()
         layout = QtWidgets.QHBoxLayout(self)
-        layout.setContentsMargins(0,0,0,0)
+        layout.setContentsMargins(0, 0, 0, 0)
 
         left_widget = QtWidgets.QWidget()
-        left_layout = QtWidgets.QHBoxLayout(left_widget)
+        left_layout = QtWidgets.QVBoxLayout(left_widget)
+
+        path_edit = QtWidgets.QLineEdit()
+        path_edit.__dict__.keys()
+        path_edit.textChanged.connect(self.update_tree)
+        self.path_edit = path_edit
 
         splitter = QtWidgets.QSplitter(QtCore.Qt.Horizontal)
         self.splitter = splitter
 
         self.setLayout(layout)
-        b = QtWidgets.QToolButton()
-        b.setText('>')
-        b.clicked.connect(self.xpand)
-        b.setMaximumWidth(20)
-        self.b = b
+        self.tool_button = QtWidgets.QToolButton()
+        self.tool_button.setText('<')
+        self.tool_button.clicked.connect(self.xpand)
+        self.tool_button.setMaximumWidth(20)
+
         self.xpanded = False
         layout.addWidget(splitter)
-        #layout.setWidgetResizable(True)
 
         nuke_folder = os.path.join(os.path.expanduser('~'), '.nuke')
-        t = browser.FileTree(nuke_folder)
-        t.path_signal.connect(self.set_file)
 
-        e = editor.Editor()
-        self.e = e
-        #for x in t, e:
-        for x in t, b, e:
-            splitter.addWidget(x)
-        #splitter.setStretchFactor(1, 2)
+        browse = browser.FileTree(nuke_folder)
+        self.browser = browse
+        left_layout.addWidget(self.path_edit)
+        left_layout.addWidget(self.browser)
+
+        #self.editor = editor.Editor()
+        self.tabs = edittabs.EditTabs()
+        self.tabs.new_tab()
+
+        widgets = [left_widget,
+                        self.tool_button,
+                        self.tabs]
+        for w in widgets:
+            splitter.addWidget(w)
+
         splitter.setSizes([200,10, 800])
-        #splitter.setStretchFactor(2, 2)
+
+        # connect signals
+        self.browser.path_signal.connect(self.set_file)
+
     def xpand(self):
         if self.xpanded:
-            symbol = '>'
+            symbol = '<'
             sizes = [200, 10, 800]  # should be current sizes
         else:
-            symbol = '<'
+            symbol = '>'
             sizes = [0, 10, 800]  # should be current sizes
 
-        self.b.setText(symbol)
+        self.tool_button.setText(symbol)
         self.splitter.setSizes(sizes)
         self.xpanded = not self.xpanded
 
+    @QtCore.Slot(str)
+    def update_tree(self, path):
+        model = self.browser.model()
+        root_path = model.rootPath()
+        if root_path in path:
+            return
+        path = os.path.dirname(path)
+        if not os.path.isdir(path):
+            return
+        path = path+os.altsep
+        print path
+        self.browser.set_model(path)
 
     @QtCore.Slot(str)
     def set_file(self, path):
+        # if no editor with path in tabs add new
+        self.editor = self.tabs.currentWidget()
         if not os.path.isfile(path):
             return
         with open(path, 'rt') as f:
-            self.e.setPlainText(f.read())
+            self.editor.setPlainText(f.read())
+            self.path_edit.setText(path)
 
 
+m = Manager()
+m.show()
 
-# m = Manager()
-# m.show()
+QtWidgets.QFileSystemModel
