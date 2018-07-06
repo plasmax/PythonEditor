f8fd8101a3794147f5f4225bd79dd1f9443a1d11
plasmax <tsalxam@gmail.com>
2018-07-06 17:01:55 +0100

    Update _init_script.py
    No need to run user init.py

--- tests/_init_script.py
+++ tests/_init_script.py
@@ -7,9 +7,4 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 if ('nuke' in globals() and nuke.GUI):
     import PythonEditor
-    # from PythonEditor.ui.nukefeatures import nukeinit
-    # nukeinit.setup()
     from PythonEditor.utils.constants import NUKE_DIR
-    execfile(os.path.join(NUKE_DIR, 'init.py'))     # executes init.py
-                                                    # found in user .nuke
-                                                    # folder
