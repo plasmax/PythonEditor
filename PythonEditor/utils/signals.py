f4762b672f1f2d1baec6cc7963876ec81ae9f384
Max Last <tsalxam@gmail.com>
2018-07-21 17:37:08 +0100

    Slightly better naming and documentation.

--- PythonEditor/utils/signals.py
+++ PythonEditor/utils/signals.py
@@ -1,17 +1,24 @@
 
 
 def connect(cls, signal, slot):
-    global __super_temp_name
-    __super_temp_name = 'banana'
+    """
+    Function to connect signals to slots and get the signature
+    of the signal in the process, i.e. "2signal(PyObject)"
+
+    TODO: instead of using a global temp variable,
+    a variable on the given class may work better.
+    """
+    global __temp_name
+    __temp_name = 'temp'
 
     def foo(x):
-        global __super_temp_name
-        __super_temp_name = x
+        global __temp_name
+        __temp_name = x
 
     connectNotify = cls.connectNotify
     cls.connectNotify = foo
     signal.connect(slot)
     cls.connectNotify = connectNotify
-    name = __super_temp_name
-    del __super_temp_name
+    name = __temp_name
+    del __temp_name
     return name, signal, slot
