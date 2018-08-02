ce9fcf1c26d85788fb053dc5b29d2418659679df
plasmax <tsalxam@gmail.com>
2018-08-02 08:20:08 +0100

    Update editor.py
    Make show mimeData more hidden, use alt to reveal

--- PythonEditor/ui/editor.py
+++ PythonEditor/ui/editor.py
@@ -243,7 +243,8 @@ class Editor(QtWidgets.QPlainTextEdit):
             e.accept()
         else:
             super(Editor, self).dragEnterEvent(e)
-
+        if e.modifiers() != QtCore.Qt.AltModifier:
+            return
         # let's see what the data contains, at least!
         # maybe restrict this to non-known formats...
         for f in mimeData.formats():
