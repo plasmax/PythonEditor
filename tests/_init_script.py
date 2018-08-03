b303752c01035e0b0d163afc5202ee66336b2b56
plasmax <tsalxam@gmail.com>
2018-08-03 18:59:55 +0100

    Update _init_script.py

--- tests/_init_script.py
+++ tests/_init_script.py
@@ -8,3 +8,4 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 if ('nuke' in globals() and nuke.GUI):
     import PythonEditor
     from PythonEditor.utils.constants import NUKE_DIR
+    PythonEditor.nuke_menu_setup(node_menu=True, nuke_menu=True)
