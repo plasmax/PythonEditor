a48a01f3ba9b82754b5c8fef15d1502ce043be2f
plasmax <tsalxam@gmail.com>
2018-08-03 18:59:25 +0100

    Update __init__.py
    new nukeinit setup

--- PythonEditor/__init__.py
+++ PythonEditor/__init__.py
@@ -1,5 +1,6 @@
 import os
 # anti-crash prevention from Nuke 11 to 10.
+bindings = 'PySide2', 'PyQt5', 'PySide', 'PyQt4'
 
 try:
     import nuke
@@ -12,7 +13,7 @@ os.environ['QT_PREFERRED_BINDING'] = pyside
 from PythonEditor.ui import ide
 
 
-def nuke_menu_setup():
+def nuke_menu_setup(nuke_menu=False, node_menu=False, pane_menu=True):
     """
     If in Nuke, setup menu.
     """
@@ -22,4 +23,5 @@ def nuke_menu_setup():
         return
 
     from PythonEditor.ui.nukefeatures import nukeinit
-    nukeinit.setup()
+    nukeinit.setup(nuke_menu=nuke_menu, node_menu=node_menu, pane_menu=pane_menu)
+
