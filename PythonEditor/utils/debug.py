5305f027183c4de3ff13e76510712a0e452ccac9
plasmax <tsalxam@gmail.com>
2018-08-10 10:42:42 +0100

    Update debug.py
    The real debug

--- PythonEditor/utils/debug.py
+++ PythonEditor/utils/debug.py
@@ -1,4 +1,19 @@
+from __future__ import print_function
+import os
+import sys
+import inspect
 
 
 def debug(*args, **kwargs):
-    pass
+    if os.getenv('USER') != 'mlast':
+        return
+
+    f = sys._getframe().f_back
+    
+    print('\nDEBUG:')
+
+    print(*args, **kwargs)
+
+    _file = inspect.getfile(f)
+    lineno = str(inspect.getlineno(f))
+    print('sublime %s:%s' % (_file, lineno))
