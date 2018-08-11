af52d62d5ccf7b6a14c72ba099e4e8f57f027911
Max Last <tsalxam@gmail.com>
2018-08-11 13:58:00 +0100

    documentation

--- PythonEditor/utils/debug.py
+++ PythonEditor/utils/debug.py
@@ -5,13 +5,17 @@ import inspect
 
 
 def debug(*args, **kwargs):
+    """
+    For exclusively printing to mlast user terminal.
+    TODO: what more useful information could be included here?
+    Would be nice to allow this to send information via email.
+    """
     if os.getenv('USER') != 'mlast':
         return
 
     f = sys._getframe().f_back
-    
-    print('\nDEBUG:')
 
+    print('\nDEBUG:')
     print(*args, **kwargs)
 
     _file = inspect.getfile(f)
