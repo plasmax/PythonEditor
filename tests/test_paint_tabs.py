72fd51831cf10015a25fe59677ef0de8408c44e4
Max Last <tsalxam@gmail.com>
2018-10-15 00:42:00 +0100

    test new paint tabs

---
+++ tests/test_paint_tabs.py
@@ -0,0 +1,46 @@
+#!/net/homes/mlast/bin nuke-safe-python-tg
+""" For testing independently. """
+from __future__ import absolute_import
+import sys
+import os
+
+
+sys.dont_write_bytecode = True
+TESTS_DIR = os.path.dirname(__file__)
+PACKAGE_PATH = os.path.dirname(TESTS_DIR)
+sys.path.append(PACKAGE_PATH)
+
+sys.path.append(TESTS_DIR+'/prototypes/')
+
+if __name__ == '__main__':
+    """
+    For testing outside of nuke.
+    """
+    try:
+        import nuke
+        pyside = ('PySide' if (nuke.NUKE_VERSION_MAJOR < 11) else 'PySide2')
+    except ImportError:
+        pyside = 'PySide'
+
+    os.environ['QT_PREFERRED_BINDING'] = pyside
+
+    from PythonEditor.ui.features import nukepalette
+    from PythonEditor.ui import ide
+    from PythonEditor.ui.Qt import QtWidgets
+
+    try:
+        app = QtWidgets.QApplication(sys.argv)
+    except RuntimeError:
+        # for running inside and outside of Nuke
+        app = QtWidgets.QApplication.instance()
+
+    import time
+    start = time.time()
+
+    import paint_qtabbar
+    app.setPalette(nukepalette.getNukePalette())
+    plastique = QtWidgets.QStyleFactory.create('Plastique')
+    QtWidgets.QApplication.setStyle(plastique)
+
+    print('PythonEditor startup time: %.04f seconds' % (time.time()-start) )
+    sys.exit(app.exec_())
