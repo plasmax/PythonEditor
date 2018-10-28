7e2d7cae56ac35bf865efc43184bee97895dac58
plasmax <tsalxam@gmail.com>
2018-10-28 20:32:48 +0000

    Update nukeinit.py

--- PythonEditor/ui/nukefeatures/nukeinit.py
+++ PythonEditor/ui/nukefeatures/nukeinit.py
@@ -15,12 +15,18 @@ from PythonEditor.ui.nukefeatures import nukedock
 reload(nukedock)
 nukedock.setup_dock()
 nukescripts.panels.__panels["i.d.e.Python_Editor"]()
+from PythonEditor.ui.nukefeatures import nukeinit
+reload(nukeinit)
 """
 
 ICON_PATH = 'PythonEditor.png'
 
 
 def setup(nuke_menu=False, node_menu=False, pane_menu=True):
+    """
+    TODO:
+    Set this up automatically based on Preferences.
+    """
     try:
         import nuke
         if not nuke.GUI:
@@ -50,41 +56,63 @@ def setup(nuke_menu=False, node_menu=False, pane_menu=True):
     if pane_menu:
         nukedock.setup_dock()
 
-        
+
+def capture_ui_state():
+    """
+    Get the current workspace layout.
+    """
+    ui_state = {}
+    pythoneditor_panel = None
+    for widget in QtWidgets.QApplication.instance().allWidgets():
+        if isinstance(widget, QtWidgets.QStackedWidget):
+            ui_state[widget] = widget.currentWidget()
+    return ui_state
+
+
+def focus_on_panel(ui_state, panel_name=PANEL_NAME):
+    panels = [p for s in ui_state.keys() for p in s.children()]
+    for stack in ui_state.keys():
+        for child in stack.children():
+            if child.objectName() == panel_name:
+                pythoneditor_panel = child
+                qt_pane = stack
+                qt_pane.setCurrentWidget(pythoneditor_panel)
+                pythoneditor_panel.setFocus(QtCore.Qt.ActiveWindowFocusReason)
+                pythoneditor_panel.activateWindow()
+                return True
+
+
 def add_to_pane():
     """
     Locates a panel and adds it to one
     of the main dock windows in order
     of preference.
     """
-    panel = None
-    for widget in QtWidgets.QApplication.instance().allWidgets():
-        if isinstance(widget, QtWidgets.QStackedWidget):
-            for child in widget.children():
-                if child.objectName() == PANEL_NAME:
-                    panel = child
-                    pane = widget
-                    break
+    ui_state = capture_ui_state()
+    if focus_on_panel(ui_state):
+        return
 
-    if bool(panel):
+    import nuke
+    from nukescripts import panels
+    found = False
 
-        pane.setCurrentWidget(panel)
-        panel.setFocus(QtCore.Qt.ActiveWindowFocusReason)
-        panel.activateWindow()
+    # this will create the PythonEditor panel and add it to the active pane
+    nuke_panel = panels.__panels.get(PANEL_NAME).__call__()
 
-    else:
-        import nuke
-        from nukescripts import panels
-        found = False
-        panel = panels.__panels.get(PANEL_NAME).__call__()
-        for dock in ['Properties.1',
-                     'Viewer.1',  'DAG.1']:
-            pane = nuke.getPaneFor(dock)
-            if pane:
-                panel.addToPane(pane)
-                found = True
-                break
-        if not found:
-            panels.__panels[PANEL_NAME]()
+    # reset Nuke ui to how it was.
+    for stack, widget in ui_state.items():
+        stack.setCurrentWidget(widget)
 
-                   
+    for dock in ['Properties.1',
+                 'Viewer.1',  'DAG.1']:
+        pane = nuke.getPaneFor(dock)
+        if pane is not None:
+            nuke_panel.addToPane(pane)
+            break
+
+    focus_on_panel(capture_ui_state())
+    for tabbar in QtWidgets.QApplication.instance().allWidgets():
+        if isinstance(tabbar, QtWidgets.QTabBar):
+            for i in range(tabbar.count()):
+                if tabbar.tabText(i) == 'Python Editor':
+                    tabbar.setCurrentIndex(i)
