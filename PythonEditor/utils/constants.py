1cb755af122717e575c9721a3aa0d47caac860e8
Max Last <tsalxam@gmail.com>
2018-12-02 18:57:47 +0000

    allow for a PYTHONEDITOR_CUSTOM_DIR

--- PythonEditor/utils/constants.py
+++ PythonEditor/utils/constants.py
@@ -12,10 +12,17 @@ if IN_NUKE:
         pyqt = 'PyQt5'
 
 USER = os.environ.get('USERNAME')
-NUKE_DIR = os.path.join(os.path.expanduser('~'), '.nuke')
+
+NUKE_DIR = None
+PYTHONEDITOR_CUSTOM_DIR = os.getenv('PYTHONEDITOR_CUSTOM_DIR')
+if PYTHONEDITOR_CUSTOM_DIR is not None:
+	if os.path.isdir(PYTHONEDITOR_CUSTOM_DIR):
+		NUKE_DIR = PYTHONEDITOR_CUSTOM_DIR
+
+if NUKE_DIR is None:
+	NUKE_DIR = os.path.join(os.path.expanduser('~'), '.nuke')
+
 QT_VERSION = os.pathsep.join([pyside, pyqt])
 
 DEFAULT_FONT = 'Courier New' if (os.name == 'nt') else 'DejaVu Sans Mono'
-
-
 
