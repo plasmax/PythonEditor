98dc5e7774fcc881fd4e57da1405b3c5ff53a59f
Max Last <tsalxam@gmail.com>
2018-12-02 19:03:20 +0000

    testing with custom test directory

--- PythonEditor/utils/constants.py
+++ PythonEditor/utils/constants.py
@@ -13,14 +13,11 @@ if IN_NUKE:
 
 USER = os.environ.get('USERNAME')
 
-NUKE_DIR = None
+NUKE_DIR = os.path.join(os.path.expanduser('~'), '.nuke')
 PYTHONEDITOR_CUSTOM_DIR = os.getenv('PYTHONEDITOR_CUSTOM_DIR')
 if PYTHONEDITOR_CUSTOM_DIR is not None:
 	if os.path.isdir(PYTHONEDITOR_CUSTOM_DIR):
 		NUKE_DIR = PYTHONEDITOR_CUSTOM_DIR
-
-if NUKE_DIR is None:
-	NUKE_DIR = os.path.join(os.path.expanduser('~'), '.nuke')
 
 QT_VERSION = os.pathsep.join([pyside, pyqt])
 
