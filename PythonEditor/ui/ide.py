a0e71cff2369b730fb3ace54a530c957b9609a24
Max Last <tsalxam@gmail.com>
2019-03-23 10:02:54 +0000

    reload modules in the order that they were loaded in

--- PythonEditor/ui/ide.py
+++ PythonEditor/ui/ide.py
@@ -1,4 +1,40 @@
 import sys
+import os
+
+PYTHON_EDITOR_MODULES = []
+
+class Finder(object):
+    """
+    Keep track of pythoneditor modules loaded
+    so that they can be reloaded in the same order.
+    """
+    _can_delete = True
+    def find_module(self, name, path=''):
+        if 'PythonEditor' not in name:
+            return
+
+        global PYTHON_EDITOR_MODULES
+        if PYTHON_EDITOR_MODULES is None:
+            return
+        if name in PYTHON_EDITOR_MODULES:
+            return
+        if path is None:
+            return
+
+        filename = name.split('.').pop()+'.py'
+        for p in path:
+            if filename in os.listdir(p):
+                PYTHON_EDITOR_MODULES.append(name)
+                return
+
+def add_to_meta_path():
+	sys.meta_path = [
+		i for i in sys.meta_path
+		if not hasattr(i, '_can_delete')
+	]
+	sys.meta_path.append(Finder())
+add_to_meta_path()
+
 import imp
 from PythonEditor.ui.Qt import QtWidgets
 from PythonEditor.ui.Qt import QtCore
@@ -33,24 +69,19 @@ class IDE(QtWidgets.QWidget):
         this module), in an order that does not
         cause errors.
         """
-        self.pythonEditor.deleteLater()
-        del self.pythonEditor
+        self.python_editor.terminal.stop()
+        self.python_editor.deleteLater()
+        del self.python_editor
 
-        not_reloadable = [
-                            'PythonEditor.ui.pythoneditor',
-                            'PythonEditor.ui.ide',
-                            '__main__'
-                         ]
+        # reload modules in the order
+        # that they were loaded in
+        for name in PYTHON_EDITOR_MODULES:
+            mod = sys.modules.get(name)
+            if mod is None:
+                continue
+            imp.reload(mod)
 
-        loaded_modules = sys.modules
-        for name, mod in loaded_modules.items():
-            if (mod and hasattr(mod, '__file__')
-                    and 'PythonEditor' in mod.__file__
-                    and name not in not_reloadable):
-                imp.reload(mod)
-
-        imp.reload(pythoneditor)
-        self.buildUI()
+        QtCore.QTimer.singleShot(1, self.buildUI)
 
     def showEvent(self, event):
         """
