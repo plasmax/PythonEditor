dc05e216567a0074de8a40cd6460cc53380d176a
Max Last <tsalxam@gmail.com>
2019-09-27 22:18:49 +0100

    tabview demo progress check in

--- scripts/prototypes/tabview_demo.py
+++ scripts/prototypes/tabview_demo.py
@@ -1,111 +1,121 @@
 
 from PythonEditor.ui.Qt.QtCore import QPropertyAnimation, Qt, Slot, QTimer
-from PythonEditor.ui.Qt.QtGui import QStandardItem
+from PythonEditor.ui.Qt.QtGui import QStandardItem, QFont
 from PythonEditor.ui.Qt.QtWidgets import (
-    QDataWidgetMapper, QWidget, QPushButton, QToolButton, QGridLayout)
+    QWidget, 
+    QSplitter, 
+    QSizePolicy,
+    QGridLayout,
+    QVBoxLayout,
+    QHBoxLayout,
+    QToolButton, 
+    QPushButton, 
+    QPlainTextEdit, 
+    QDataWidgetMapper, 
+)
 
 from PythonEditor.ui.editor import Editor
 from PythonEditor.ui.tabview import TabView, ListView
 from PythonEditor.models.xmlmodel import XMLModel
 
 
-class Window(QWidget):
+class TabEditor(QWidget):
     def __init__(self):
-        super(Window, self).__init__()
+        """ Brings together the data model (currently XMLModel),
+        along with several views - Editor, TabView, ListView, and QDataWidgetMapper. 
+        """
+        super(TabEditor, self).__init__()
         self.model = XMLModel()
 
-        self.tabBar = TabView()
-        #self.textEdit = QTextEdit()
-        self.textEdit = Editor()
-        #self.textEdit = QPlainTextEdit()
-        #self.textEdit.setFont(QFont('DejaVu Sans'))
-        self.listView = ListView()
-        #self.treeView = QTreeView()
-        #self.tableView = QTableView()
-        # self.nextButton = QPushButton("&Next")
-        # self.previousButton = QPushButton("&Previous")
+        # create widgets
+        self.tab_view = TabView()
+
+        self.text_edit = Editor()
+        self.text_edit.setFont(QFont('DejaVu Sans'))
+        
+        self.list_view = ListView()
+        self.list_view.setMaximumWidth(0)
+
         self.listButton = QToolButton()
-        self.listButton.setArrowType(Qt.RightArrow)
-
-        #b = QToolButton()
+        self.listButton.setArrowType(Qt.DownArrow)
         self.listButton.setFixedSize(24, 24)
-        self.mapper = QDataWidgetMapper(self)
-
-        self.listView.setMaximumWidth(0)
-        #self.listView.setAcceptsDrops(True)
-        #self.listView.setMovement(QListView.Snap)
-
-        self.textEdit.setMaximumHeight(1000)
-        self.listView.setModel(self.model)
-
-        #self.treeView.setModel(self.model)
-        #self.tableView.setModel(self.model)
-
-        self.tabBar.setModel(self.model)
-
-        self.mapper.setModel(self.model)
-        self.mapper.addMapping(self.textEdit, 1)
-
-        self.selection_model = self.listView.selectionModel()
-        self.selection_model.currentRowChanged.connect(self.mapper.setCurrentModelIndex)
-        self.tabBar.currentChanged.connect(self.mapper.setCurrentIndex)
-        self.tabBar.setSelectionModel(self.selection_model)
-        self.textEdit.modificationChanged.connect(self.submit_and_refocus)
-        self.mapper.currentIndexChanged.connect(self.tabBar.setCurrentIndex)
-        self.mapper.currentIndexChanged.connect(self.setListIndex)
-        #self.addressEdit.textChanged.connect(self.mapper.submit)
-        # self.nextButton.clicked.connect(self.mapper.toNext)
-        # self.previousButton.clicked.connect(self.mapper.toPrevious)
-        # self.mapper.currentIndexChanged.connect(self.updateButtons)
-        self.model.row_moved.connect(self.tabBar.swap_tabs)
-        #self.model.row_moved.connect(self.tabBar.moveTab)
-
-        self.listButton.clicked.connect(self.animate_listview)
-
-        layout = QGridLayout(self)
-        layout.addWidget(self.listButton, 0, 0)
-        layout.addWidget(self.listView, 1, 0)
-        layout.addWidget(self.tabBar, 0, 1, 1, 1, Qt.AlignTop)
-        layout.addWidget(self.textEdit, 1, 1, 1, 1)
+        
+        # layout
+        self.tab_widget = QWidget(self)
+        tab_layout = QHBoxLayout(self.tab_widget)
+        tab_layout.setContentsMargins(0,0,0,0)
+        tab_layout.addWidget(self.tab_view)
+        tab_layout.addWidget(self.listButton)
+        
+        self.splitter = QSplitter(Qt.Horizontal)
+        self.splitter.addWidget(self.text_edit)
+        self.splitter.addWidget(self.list_view)
+        policy = QSizePolicy.Expanding
+        self.splitter.setSizePolicy(policy, policy)
+        
+        layout = QVBoxLayout(self)
+        layout.setContentsMargins(0,0,0,0)
+        layout.addWidget(self.tab_widget)
+        layout.addWidget(self.splitter)
         self.setLayout(layout)
 
-        self.setWindowTitle("Simple Widget Mapper")
-        self.mapper.toFirst()
-        QTimer.singleShot(1000, self.animate_listview)
+        # set model on views
+        self.list_view.setModel(self.model)
+        self.tab_view.setModel(self.model)
+        
+        # add model mapper
+        self.mapper = QDataWidgetMapper(self)
+        self.mapper.setModel(self.model)
+        self.mapper.addMapping(self.text_edit, 1)
 
-    def animate_listview(self, start=0, end=300):
-        if self.listView.maximumWidth() != 0:
-            start, end = end, start
-            self.listButton.setArrowType(Qt.RightArrow)
-        else:
-            self.listButton.setArrowType(Qt.DownArrow)
+        # connect signals
+        self.selection_model = self.list_view.selectionModel()
+        self.selection_model.currentRowChanged.connect(self.mapper.setCurrentModelIndex)
+        self.tab_view.currentChanged.connect(self.mapper.setCurrentIndex)
+        self.tab_view.setSelectionModel(self.selection_model)
+        self.text_edit.modificationChanged.connect(self.submit_and_refocus)
+        self.mapper.currentIndexChanged.connect(self.tab_view.setCurrentIndex)
+        self.mapper.currentIndexChanged.connect(self.setListIndex)
+        self.model.row_moved.connect(self.tab_view.swap_tabs)
+        self.listButton.clicked.connect(self.animate_listview)
+        
+        # get first item from model - this is where we would restore last viewed tab
+        self.mapper.toFirst()
+
+    def animate_listview(self, start=0, end=100):
         anim = QPropertyAnimation(
-            self.listView,
+            self.list_view,
             'maximumWidth'
         )
         self._anim = anim
+        
+        def release():
+            self.list_view.setMaximumWidth(2000)
+        if self.list_view.maximumWidth() != 0:
+            start, end = end, start
+            self.listButton.setArrowType(Qt.DownArrow)
+        else:
+            self.listButton.setArrowType(Qt.RightArrow)
+            anim.finished.connect(release)
+            
         anim.setStartValue(start)
         anim.setEndValue(end)
-        anim.setDuration(300)
+        anim.setDuration(80)
         anim.start()
 
     @Slot()
     def submit_and_refocus(self):
-        cursor = self.textEdit.textCursor()
+        cursor = self.text_edit.textCursor()
         self.mapper.submit()
-        self.textEdit.setTextCursor(cursor)
+        self.text_edit.setTextCursor(cursor)
 
     @Slot(int)
     def setListIndex(self, row):
         index = self.model.index(row, 0)
-        self.listView.setCurrentIndex(index)
-
-    # @Slot(int)
-    # def updateButtons(self, row):
-        # self.previousButton.setEnabled(row > 0)
-        # self.nextButton.setEnabled(row < self.model.rowCount()-1)
+        self.list_view.setCurrentIndex(index)
 
 
 if __name__ == '__main__':
-    w = Window()
+    w = TabEditor()
     w.show()
+    w.resize(800, 400)
