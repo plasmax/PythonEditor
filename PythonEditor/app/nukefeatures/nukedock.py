e8e0ce348a95a08c3273569260936ebfd55bc188
plasmax <tsalxam@gmail.com>
2020-09-19 00:00:13 +0100

    Update nukedock.py
    documentation

--- PythonEditor/app/nukefeatures/nukedock.py
+++ PythonEditor/app/nukefeatures/nukedock.py
@@ -1,19 +1,18 @@
 
 
 def setup_dock(shortcut=None):
-    """ Register the PythonEditor interface
+    """Register the PythonEditor interface
     as a Nuke docked panel.
     """
-    try:
-        import nuke
-        from nukescripts import PythonPanel, registerPanel
-    except ImportError:
-        return
+    import nuke
+    from nukescripts import PythonPanel, registerPanel
 
     # register the panel manually, expressly for
     # the purpose of redefining the nested addToPane
     # function to take a pane parameter.
     class Panel(PythonPanel):
+        # This class was previously nested within
+        # the nukescripts.registerWidgetAsPanel function.
         def __init__(self, widget, name, _id):
             PythonPanel.__init__(self, name, _id)
             self.custom_knob = nuke.PyCustom_Knob(
@@ -25,8 +24,23 @@ def setup_dock(shortcut=None):
     widget = '__import__("PythonEditor.ui.ide", fromlist=["IDE"]).IDE'
     name = 'Python Editor'
     _id = 'Python.Editor'
+
     def add_panel(pane=None):
-        return Panel(widget, name, _id).addToPane(pane=pane)
+        """Add or move existing PythonEditor
+        panel to a new pane. This is what
+        makes the PythonEditor a singleton.
+        
+        :param pane: Nuke <type 'Dock'>
+
+        TODO: maybe not here, but this should trigger the "in focus" signal that overrides shortcuts
+        """
+        import PythonEditor
+        try:
+            panel = PythonEditor.__dock
+        except AttributeError:
+            panel = Panel(widget, name, _id)
+            PythonEditor.__dock = panel
+        return panel.addToPane(pane=pane)
 
     menu = nuke.menu('Pane')
     menu.addCommand(name, add_panel, shortcut=shortcut)
