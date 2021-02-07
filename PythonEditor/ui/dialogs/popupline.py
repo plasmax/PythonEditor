8f9b117bd9f2c6978d54ca4e7fb15179804adc79
Max Last <tsalxam@gmail.com>
2021-02-07 18:15:46 +0000

    trim whitespace, letters highlight in white

--- PythonEditor/ui/dialogs/popupline.py
+++ PythonEditor/ui/dialogs/popupline.py
@@ -6,7 +6,7 @@ try:
 except ImportError:
     from PySide.QtGui import *
     from PySide.QtCore import *
-    
+
 
 from PythonEditor.utils.search import nonconsec_find
 from PythonEditor.utils.goto import goto_position
@@ -26,7 +26,7 @@ class SymbolModel(QStandardItemModel):
         self.editor = editor
         self.current_lineno = editor.textCursor().blockNumber()+1
         self.populate_flat()
-        
+
     def populate_flat(self):
         row = 0
         for lineno, text, indent in self.get_symbols():
@@ -52,7 +52,7 @@ class SymbolModel(QStandardItemModel):
                 previous_item = item
             parent.appendRow([item])
             self._previous_indent = indent
-        
+
     def get_symbols(self):
         whole_text = self.editor.toPlainText()
         for lineno, text in enumerate(whole_text.splitlines()):
@@ -80,11 +80,11 @@ class ProxyModel(QSortFilterProxyModel):
         self.setFilterCaseSensitivity(Qt.CaseSensitivity.CaseInsensitive)
         self._pattern = ''
         self.setFilterKeyColumn(0)
-    
+
     def setFilterRegExp(self, pattern):
         self._pattern = str(pattern).lower()
         super(ProxyModel, self).setFilterRegExp(pattern)
-    
+
     def filterAcceptsRow(self, row, parent):
         if not self._pattern:
             return True
@@ -93,7 +93,7 @@ class ProxyModel(QSortFilterProxyModel):
         index = model.index(row, self.filterKeyColumn(), parent)
         found = self.index_match(index)
         return found
-    
+
     def index_match(self, index):
         text = index.data(Qt.DisplayRole)
         needle = str(self._pattern).lower()
@@ -105,7 +105,7 @@ class Popup(QWidget):
     def showEvent(self, event):
         super(Popup, self).showEvent(event)
         self.place_over_parent()
-    
+
     def place_over_parent(self):
         parent = self.parent()
         self.resize(parent.width()/2, self.height())
@@ -118,18 +118,18 @@ class CodeDelegate(QStyledItemDelegate):
     def __init__(self, parent=None):
         super(CodeDelegate, self).__init__(parent=parent)
         self.filter_text = ''
-        
+
     def sizeHint(self, option, index):
         size = super(CodeDelegate, self).sizeHint(option, index)
         size.setHeight(size.height()+6)
         return size
 
     def paint(self, painter, option, index):
-        
+
         # don't draw the default focus border
         if option.state & QStyle.State_HasFocus:
             option.state &= ~ QStyle.State_HasFocus
-        
+
         if not self.filter_text:
             return super(CodeDelegate, self).paint(painter, option, index)
         self.paint_base(painter, option, index)
@@ -166,7 +166,7 @@ class CodeDelegate(QStyledItemDelegate):
         # checkRect = style.proxy().subElementRect(QStyle.SE_ItemViewItemCheckIndicator, vopt, widget)
         # iconRect = style.proxy().subElementRect(QStyle.SE_ItemViewItemDecoration, vopt, widget)
         # textRect = style.proxy().subElementRect(QStyle.SE_ItemViewItemText, vopt, widget)
-        
+
         ## draw the background
         # style.proxy().drawPrimitive(QStyle.PE_PanelItemViewItem, opt, p, widget)
         ## draw the check mark
@@ -174,7 +174,7 @@ class CodeDelegate(QStyledItemDelegate):
             # option = QStyleOptionViewItem(vopt)
             # option.rect = checkRect
             # option.state = option.state & ~QStyle.State_HasFocus
-            
+
             # if vopt.checkState == Qt.Unchecked:
                 # option.state |= QStyle.State_Off
             # elif vopt.checkState == Qt.PartiallyChecked:
@@ -192,7 +192,7 @@ class CodeDelegate(QStyledItemDelegate):
                 # mode = QIcon.Selected
             # state = QIcon.On if vopt.state & QStyle.State_Open else QIcon.Off
             # vopt.icon.paint(p, iconRect, vopt.decorationAlignment, mode, state)
-            
+
             ## draw the text
             # if not vopt.text.isEmpty():
                 # cg = QPalette.Normal if (vopt.state & QStyle.State_Enabled) else QPalette.Disabled
@@ -224,45 +224,38 @@ class CodeDelegate(QStyledItemDelegate):
 
     def paint_each_letter(self, painter, option, index):
 
+        painter.save()
         pattern = list(set(self.filter_text))
         text = index.data()
 
         font = painter.font()
         rect = QRect(option.rect)
-        
+
         # little adjustments needed to match normal text placement - because of padding, I think?? maybe
         rect.moveRight(rect.right()+3)
         rect.moveTop(rect.top()+3)
-        
+
         prev_found = False
         for letter in text:
 
             found = letter.lower() in pattern
             same_as_prev = found is prev_found
             prev_found = found
-            if not same_as_prev:               
+            if not same_as_prev:
                 if found:
-                    pen = QPen(option.palette.highlightedText(), 1)
+                    pen = QPen(QBrush(Qt.white), 2)
                 else:
                     pen = QPen(option.palette.text(), 1)
                 painter.setPen(pen)
                 font.setBold(found)
                 painter.setFont(font)
-                
-                # hack for extra bold
-                if found:
-                    painter.drawText(rect, letter)
-                    painter.drawText(rect, letter)
-                    painter.drawText(rect, letter)
-                    painter.drawText(rect, letter)
-                    painter.drawText(rect, letter)
-                    painter.drawText(rect, letter)
-            
+
             painter.drawText(rect, letter)
-            
+
             letter_width = QFontMetrics(font).size(Qt.TextSingleLine, letter).width()
             rect.adjust(letter_width, 0, 0, 0)
-        
+        painter.restore()
+
     def draw_rich_text(self, painter, option, index):
         html = index.data()
         td = QTextDocument()
@@ -275,7 +268,7 @@ class CodeDelegate(QStyledItemDelegate):
         painter.setFont(option.font)
         painter.translate(option.rect.topLeft())
         td.drawContents(painter)
-    
+
     def set_filter_text(self, text):
         self.filter_text = text.lower()
 
@@ -289,7 +282,7 @@ class Tree(QTreeView):
         self.setItemDelegate(CodeDelegate(self))
         # self.setAutoExpandDelay(0)
         self.set_highlight_color()
-    
+
     def set_highlight_color(self):
         palette = self.palette()
         brush = palette.highlight()
@@ -298,7 +291,7 @@ class Tree(QTreeView):
         brush.setColor(color)
         palette.setBrush(QPalette.Highlight, brush)
         self.setPalette(palette)
-    
+
     @Slot(str)
     def select_first_index(self, _):
         self.select_index(self.model().index(0,0))
@@ -306,7 +299,7 @@ class Tree(QTreeView):
     @Slot(int)
     def select_row(self, row):
         self.select_index(self.model().index(row, 0))
-        
+
     def select_index(self, index):
         smodel = self.selectionModel()
         smodel.setCurrentIndex(index, QItemSelectionModel.ClearAndSelect)
@@ -327,26 +320,26 @@ class CloseIconPainter(QIconEngine):
         rect = QRect(QPoint(0.0,0.0), size)
         self.paint(painter, rect, mode, state)
         return pix
-   
+
     def paint(self, painter, rect, mode=QIcon.Mode.Normal, state=QIcon.State.Off):
         h = rect.height()/2
         close_rect = QRect(0,0,h,h)
         close_rect.moveCenter(rect.center())
-        
+
         left_line = QLine(close_rect.topLeft(), close_rect.bottomRight())
         right_line = QLine(close_rect.topRight(), close_rect.bottomLeft())
-        
+
         painter.setRenderHint(QPainter.RenderHint.HighQualityAntialiasing)
-        
+
         if mode == QIcon.Disabled:
             # paint me a sunken X
             brush = QBrush(QColor.fromRgbF(1,0.2,0.2,1))
             painter.setBrush(brush)
-            
+
             h = rect.height()/2
             circle_rect = QRect(0,0,h,h)
             circle_rect.moveCenter(rect.center())
-            
+
             # paint me a filled circle o
             # for the "not saved"/"document modified" state
             paint_x = False
@@ -365,16 +358,16 @@ class CloseIconPainter(QIconEngine):
             # x_rect = QRect(0,0,h,h)
             # x_rect.moveCenter(rect.center())
             # painter.fillRect(x_rect, QColor.fromRgbF(1,1,1,0.12))
-            
+
             brush = QPalette().light()
             pen = QPen(brush, 2)
             painter.setPen(pen)
             rect.moveCenter(rect.center()+QPoint(0.5,0.5))
-        
+
         # paint me a normal X
         painter.drawLine(left_line)
         painter.drawLine(right_line)
-        
+
 
 class CloseIcon(QIcon):
     def __init__(self):
@@ -394,7 +387,7 @@ class Line(QLineEdit):
         except TypeError:
             # not worth supporting on PySide
             pass
-        
+
     def eventFilter(self, obj, event):
         if obj == self.btn:
             # trick the QToolButton into having a Hover state by hijacking the Disabled state.
@@ -405,11 +398,11 @@ class Line(QLineEdit):
                 # self.btn.setDown(False)
                 self.btn.setEnabled(False)
         return False
-        
+
     def closeEvent(self, event):
         super(Line, self).closeEvent(event)
         self.btn.removeEventFilter(self)
-        
+
     def add_close_action(self):
         self.close_action = QAction()
         icon = CloseIcon()
@@ -421,7 +414,7 @@ class Line(QLineEdit):
         self.btn = self.children()[1]
         self.btn.setEnabled(False)
         self.btn.installEventFilter(self)
-        
+
     def set_text_from_index(self, selected, deselected):
         for index in selected.indexes():
             text = index.data()
@@ -432,27 +425,27 @@ class Line(QLineEdit):
 class PopupLine(Popup):
     def __init__(self, editor):
         super(PopupLine, self).__init__(parent=editor)
-        
+
         self.editor = editor
         self.current_scroll = editor.verticalScrollBar().value()
         self.line_edit = Line()
         self.line_edit.setFont(editor.font())
         self.tree_view = Tree()
         self.tree_view.setFont(editor.font())
-        
+
         self.setFocusProxy(self.line_edit)
         self.tree_view.setFocusProxy(self.line_edit)
-        
+
         self.editor.installEventFilter(self)
         self.line_edit.installEventFilter(self)
         self.tree_view.installEventFilter(self)
-        
+
         self.symbol_model = SymbolModel()
         self.proxy_model = ProxyModel()
         self.proxy_model.setSourceModel(self.symbol_model)
         self.tree_view.setModel(self.proxy_model)
         # TODO: set tree_view current selection to the first symbol above the current line using LINENO_ROLE
-        
+
         self.setLayout(QVBoxLayout(self))
         layout = self.layout()
         layout.addWidget(self.line_edit)
@@ -467,30 +460,30 @@ class PopupLine(Popup):
         self.symbol_model.loading_complete.connect(self.tree_view.select_row)
         # delay this connection so we don't jump straight to the first
         QTimer.singleShot(10, self.connect_goto_signal)
-        
+
         # fill with data
         self.symbol_model.populate_from_editor(editor)
-        
+
     def connect_goto_signal(self):
         smodel = self.tree_view.selectionModel()
         smodel.selectionChanged.connect(self.line_edit.set_text_from_index)
         smodel.selectionChanged.connect(self.goto_line)
-    
+
         # if we have a selection, use it
         self.line_edit.setText(self.editor.selected_text())
         self.line_edit.selectAll()
-        
+
     def showEvent(self, event):
         super(PopupLine, self).showEvent(event)
         self.line_edit.setFocus(Qt.TabFocusReason)
-        
+
     def closeEvent(self, event):
         self.editor.removeEventFilter(self)
         self.line_edit.removeEventFilter(self)
         self.tree_view.removeEventFilter(self)
         self.editor.setFocus(Qt.MouseFocusReason)
         super(PopupLine, self).closeEvent(event)
-        
+
     def eventFilter(self, obj, event):
         if obj == self.editor:
             if event.type() == QEvent.Move:
@@ -504,7 +497,7 @@ class PopupLine(Popup):
                 self.line_edit.setFocus(Qt.TabFocusReason)
             # elif event.type() == QEvent.MouseButtonPress: # TODO: click on item to close it? maybe
                 # self.close()
-            
+
         if event.type() == QEvent.KeyPress:
             if event.key() == Qt.Key_Escape:
                 self.cancel()
@@ -517,24 +510,24 @@ class PopupLine(Popup):
                     self.tree_view.keyPressEvent(event)
                     return True
         return False
-    
+
     def goto_line(self, selected, deselected):
         for index in selected.indexes():
             lineno = index.data(LINENO_ROLE)
             goto_line(self.editor, lineno, scroll=True)
             break
-    
+
     def cancel(self):
         self.close()
-        
+
         # restore everything to how it was
         goto_line(self.editor, self.symbol_model.current_lineno, scroll=False)
         self.editor.verticalScrollBar().setValue(self.current_scroll)
 
 
-        
+
 if __name__ == '__main__':
-    
+
     # test
     popup = PopupLine(_ide.python_editor.editor)
     popup.show()
