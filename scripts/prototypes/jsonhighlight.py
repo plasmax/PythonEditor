fd4fd4132bdf95c635b256771193cfd06a271c9b
Max Last <tsalxam@gmail.com>
2021-02-21 22:56:28 +0000

    faster json highlighter

--- scripts/prototypes/jsonhighlight.py
+++ scripts/prototypes/jsonhighlight.py
@@ -1,45 +1,51 @@
+from Qt.QtGui import QSyntaxHighlighter, QColor
+
+
+PURPLE = QColor.fromRgbF(0.7, 0.5, 1, 1)
+BLUE = QColor.fromRgbF(0, 0.5, 1, 1)
+
+
 class JSONHighlighter(QSyntaxHighlighter):
-    rules = {
-        "([-0-9.]+)(?!([^\"]*\"[\\s]*\\:))": Qt.darkRed,
-        "(?:[ ]*\\,[ ]*)(\"[^\"]*\")": Qt.darkGreen,
-        "(\"[^\"]*\")\\s*\\": Qt.darkGreen,
-        "(\"[^\"]*\")\\s*\\:": QColor.fromRgbF(0.7,0.5,1,1),
-        ":+(?:[: []*)(\"[^\"]*\")": QColor.fromRgbF(0,0.5,1,1),
-    }
-    rules = [[QRegExp(pat), fmt] for pat, fmt in rules.items()]
-    
-    def __init__(self, document):
-        super(JSONHighlighter, self).__init__(document)
-   
-        rules = []
+    def highlightBlock(self, s):
+        if not s.strip():
+            return
+        i = 0
+        for start, length in get_string_ranges(s):
+            if i % 2:
+                color = BLUE
+            else:
+                color = PURPLE
+            i += 1
+            self.setFormat(start, length, color)
 
-        # numbers
-        rule = QRegExp("([-0-9.]+)(?!([^\"]*\"[\\s]*\\:))")
-        rules.append((rule, Qt.darkRed))
 
-        # middle
-        rule = QRegExp("(?:[ ]*\\,[ ]*)(\"[^\"]*\")")
-        rules.append((rule, Qt.darkGreen))
+def get_string_ranges(t):
+    """Get the in and out points of double-quote encased strings."""
 
-        # last
-        rule = QRegExp("(\"[^\"]*\")(?:\\s*\\])")
-        rules.append((rule, Qt.darkGreen))
+    # life's too short to parse escape characters.
+    s = t.replace('\\"', '##')
+    assert len(s) == len(t)
 
-        # string
-        rule = QRegExp("(\"[^\"]*\")\\s*\\:")
-        rules.append((rule, QColor.fromRgbF(0.7,0.5,1,1)))
+    if not s.strip():
+        return []
+    i = 0
+    prev_c = ''
+    in_str = False
+    length = 0
+    for i in range(len(s)):
+        c = s[i]
 
-        rule = QRegExp(":+(?:[: []*)(\"[^\"]*\")")
-        rules.append((rule, QColor.fromRgbF(0,0.5,1,1)))
+        if in_str:
+            length += 1
 
-        self.rules = [[QRegExp(pat), fmt] for pat, fmt in rules]
-        
-    def highlightBlock(self, text):
-        # object
-        for exp, fmt in self.rules:
-            index = exp.indexIn(text)
-            while (index >= 0):
-                index = exp.pos(1);
-                length = len(exp.cap(1))
-                self.setFormat(index, length, fmt)
-                index = exp.indexIn(text, index + length)
+        if c == '\"':
+            if in_str:
+                # we've reached the end of the string
+                in_str = False
+                yield i-length+1, length-1
+                length = 0
+            else:
+                in_str = True
+
+        prev_c = c
+        i += 1
