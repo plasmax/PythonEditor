2829b96329f83d60f9d4d0da41defdca8eadc9ab
Max Last <tsalxam@gmail.com>
2021-03-14 23:22:52 +0000

    copied over a test from actions.py

---
+++ tests/test_actions.py
@@ -0,0 +1,20 @@
+from pytestqt import qtbot
+from PythonEditor.ui.features import actions
+from PythonEditor.ui.Qt import QtWidgets, QtGui
+
+def test_toggle_backslashes_in_string():
+    test_path = "c:\\path/to\\some\\file.jpg"
+    result = actions.toggle_backslashes_in_string(test_path)
+    assert result == "c:/path/to/some/file.jpg"
+
+
+def test_toggle_backslashes(qtbot):
+    editor = QtWidgets.QPlainTextEdit()
+    test_path = "c:\\path/to\\some\\file.jpg"
+    editor.setPlainText(test_path)
+    textCursor = editor.textCursor()
+    textCursor.setPosition(0, QtGui.QTextCursor.MoveAnchor)
+    editor.setTextCursor(textCursor)
+    actions.toggle_backslashes(editor)
+    assert editor.toPlainText() == "c:/path/to/some/file.jpg"
+
