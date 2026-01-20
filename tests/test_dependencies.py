89001e37484e1fb3cc618535b52e030a242f1686
Max Last <tsalxam@gmail.com>
2026-01-20 16:34:48 +0000

    adjust test

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -3,7 +3,7 @@ def test_cElementTree():
    from xml.etree import cElementTree
 
 def test_Qt():
-    from Qt import QtWidgets
+    from PythonEditor.ui.Qt import QtWidgets
 
 def test_six():
     from six import string_types
