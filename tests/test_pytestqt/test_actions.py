35a8768310142bdfe4d1fd4fc1753b9dee1ba7c7
Max Last <tsalxam@gmail.com>
2026-01-20 17:35:49 +0000

    all tests passing

--- tests/test_pytestqt/test_actions.py
+++ tests/test_pytestqt/test_actions.py
@@ -1,3 +1,5 @@
+import sys
+
 from PythonEditor.ui.features import actions
 from PythonEditor.ui.Qt import QtWidgets, QtGui
 
@@ -8,6 +10,12 @@ def test_toggle_backslashes_in_string():
 
 
 def test_toggle_backslashes():
+    created_app = False
+    app = QtWidgets.QApplication.instance()
+    if app is None:
+        app = QtWidgets.QApplication(sys.argv)
+        created_app = True
+
     editor = QtWidgets.QPlainTextEdit()
     test_path = "c:\\path/to\\some\\file.jpg"
     editor.setPlainText(test_path)
@@ -16,4 +24,9 @@ def test_toggle_backslashes():
     editor.setTextCursor(textCursor)
     actions.toggle_backslashes(editor)
     assert editor.toPlainText() == "c:/path/to/some/file.jpg"
+
+    editor.close()
+    QtWidgets.QApplication.processEvents()
+    if created_app:
+        app.quit()
 
